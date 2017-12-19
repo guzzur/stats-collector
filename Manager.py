67f18f76903563dbfb756e7ab6a6d580ff0e428d
Guzzur <frazykov@gmail.com>
2017-12-20 00:37:40 +0200

    Initial last minutes goals query

--- Manager.py
+++ Manager.py
@@ -1,7 +1,8 @@
 from ConfigManager import ConfigManager
 from StatsCollector import StatsCollector
+import unicodedata
 
-delimeter = "-------------------------------------"
+delimeter = "-" * 82
 
 
 def print_league_table(league):
@@ -26,22 +27,21 @@ def print_game_goals(game):
     pass
 
 
-def team_breaked_lead(game, team):
+def team_broke_lead(game, home_team_name, away_team_name):
     bBrokeLead = False
-    if team in home_team_name or team in away_team_name:
-        for event in game_events["incidents"]:
-            if event["incidentType"] == "goal":
-                if event["homeScore"] == event["awayScore"]:
-                    bBrokeLead = True
-                    if event["scoringTeam"] == 1:
-                        team_name = home_team_name
-                    else:
-                        team_name = away_team_name
-                    print "\t" + str(event["time"]) + "' (" + team_name + ") " + str(
-                        event["homeScore"]) + " : " + str(event["awayScore"])
-        if bBrokeLead:
-            print_game_result(game)
-            print delimeter
+    for event in game_events["incidents"]:
+        if event["incidentType"] == "goal":
+            if event["homeScore"] == event["awayScore"]:
+                bBrokeLead = True
+                if event["scoringTeam"] == 1:
+                    team_name = home_team_name
+                else:
+                    team_name = away_team_name
+                print "\t" + str(event["time"]) + "' (" + team_name + ") " + str(
+                    event["homeScore"]) + " : " + str(event["awayScore"])
+    if bBrokeLead:
+        print_game_result(game)
+        print delimeter
 
 
 def minimum(a, b):
@@ -72,13 +72,31 @@ if __name__ == "__main__":
             league_start_time = holder[2]["started"]
             games = collector.get_games(league_start_time)
 
+            team = "Real Sociedad"
+            print_heading("DETAILED DESCRIPTION: " + team.upper())
             print_heading("LEAD BREAKS")
+
             for game in games["weekMatches"]["tournaments"][0]["events"]:
                 game_events = collector.get_dt_games(str(game["id"]))
 
-                home_team_name = u"".join(game["homeTeam"]["name"])
-                away_team_name = u"".join(game["awayTeam"]["name"])
+                home_team_name = unicodedata.normalize('NFKD', game["homeTeam"]["name"]).encode('ascii','ignore')
+                away_team_name = unicodedata.normalize('NFKD', game["awayTeam"]["name"]).encode('ascii','ignore')
 
-                team = "Sociedad"
-                team_breaked_lead(game, team)
+                if team.lower() in home_team_name.lower() or team.lower() in away_team_name.lower():
+                    #team_broke_lead(game, home_team_name, away_team_name)
+
+                    bBrokeLead = False
+                    for event in game_events["incidents"]:
+                        if event["incidentType"] == "goal":
+                            if event["homeScore"] == event["awayScore"]:
+                                bBrokeLead = True
+                                if event["scoringTeam"] == 1:
+                                    team_name = home_team_name
+                                else:
+                                    team_name = away_team_name
+                                print "\t" + str(event["time"]) + "' (" + team_name + ") " + str(
+                                    event["homeScore"]) + " : " + str(event["awayScore"])
+                    if bBrokeLead:
+                        print_game_result(game)
+                        print delimeter
 
